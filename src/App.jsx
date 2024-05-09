f4450e2511e91960b328b91a5516fdda5f14bf18
mfadhili <mfadhili@github.com>
2024-05-10 02:37:03 +0300

    Error page

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Cart from "./features/cart/Cart.jsx";
 import Order from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import AppLayout from "./ui_components/AppLayout.jsx";
+import Error from "./ui_components/Error.jsx";
 
 const router = createBrowserRouter([
     {
         element: <AppLayout/>,
+        errorElement: <Error />,
         children: [
             { path: '/', element: <Home /> },
             { path: '/menu', element: <Menu />, loader: loaderMenu, },
